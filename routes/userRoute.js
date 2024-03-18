4077314af074b5dfcd3c14254dc49a476a04f51f
ajay sonere <ajaysonere786@gmail.com>
2024-03-18 11:17:55 +0530

    deploy

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,11 +5,11 @@ const userRouter = express.Router();
 
 userRouter.post("/register" , register);
 userRouter.post("/login" , Login);
+userRouter.get("/verify-email/:token", verifyEmail);
 userRouter.get("/get-user/:id" , getUser);
 userRouter.patch("/update-user/:id" , updateUser)
 userRouter.post("/forget-password" ,forgetPassword );
 userRouter.post("/reset-password" , resetPassword);
-userRouter.post("/verify-email/:token", verifyEmail);
 
 
 export default userRouter;
